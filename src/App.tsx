d3b4c255c9b2e7a04faf33185a0b1688da234c35
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2023-09-27 18:33:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/Button"
+
+function App() {
+
+  return (
+    <div>
+      <Button variant={"success"}/>
+      <Button variant={"danger"}/>
+      <Button variant={"primary"}/>
+      <Button variant={"secondary"}/>
+    </div>
+  ) 
+}
+
+export default App
